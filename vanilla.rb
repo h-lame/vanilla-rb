7672349036cb53967b19c858b7b867b1f2ae8915
James Adam <james@lazyatom.com>
2008-03-05 22:46:21 +0000

    Make the present method available to other web frameworks

--- vanilla.rb
+++ vanilla.rb
@@ -10,3 +10,21 @@ ActiveRecord::Base.establish_connection(
   :adapter => 'sqlite3',
   :database => 'soup_development.db'
 )
+
+module Vanilla
+  def self.present(params, snip_name, part=nil)
+    case params[:format]
+    when 'html'
+      # render in main template
+      Vanilla::Render.render('system', :main_template, [], params, Vanilla::Render::Erb)
+    when 'raw'
+      # Return the raw content of the snip (or snip part)
+      Vanilla::Render.render(snip_name, part || :content, [], params, Vanilla::Render::Raw)
+    when 'text'
+      # Render the content of this snip, but without recursing into other snips
+      Vanilla::Render.render_without_including_snips(snip_name, part || :content, [], params)
+    else
+      "Unknown format '#{params[:format]}'"
+    end
+  end
+end
--- vanilla/snips_on_sinatra.rb
+++ vanilla/snips_on_sinatra.rb
@@ -13,26 +13,8 @@ ActiveRecord::Base.establish_connection(
 Sinatra::StaticEvent::MIME_TYPES.merge!({'' => 'text/plain'})
 Sinatra::StaticEvent::MIME_TYPES.merge!({'js' => 'text/javascript'})
 
-helpers do
-  def present(snip_name, part=nil)
-    case params[:format]
-    when 'html'
-      # render in main template
-      Vanilla::Render.render('system', :main_template, [], params, Vanilla::Render::Erb)
-    when 'raw'
-      # Return the raw content of the snip (or snip part)
-      Vanilla::Render.render(snip_name, part || :content, [], params, Vanilla::Render::Raw)
-    when 'text'
-      # Render the content of this snip, but without recursing into other snips
-      Vanilla::Render.render_without_including_snips(snip_name, part || :content, [], params)
-    else
-      "Unknown format '#{params[:format]}'"
-    end
-  end  
-end
-
 static('/public', 'vanilla/public')
 
 get('/') { redirect Vanilla::Routes.url_to('start') }
-get('/:snip') { present params[:snip] }
-get('/:snip/:part') { present params[:snip], params[:part] }
+get('/:snip') { Vanilla.present params, params[:snip] }
+get('/:snip/:part') { Vanilla.present params, params[:snip], params[:part] }
