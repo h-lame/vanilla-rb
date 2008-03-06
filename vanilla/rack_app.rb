9330abdba8f6d7af0cb398bdf439c113bf797215
James Adam <james@lazyatom.com>
2008-03-06 23:46:16 +0000

    Added a fix for the weirdness on Dreamhost where request.path_info was empty

--- vanilla/rack_app.rb
+++ vanilla/rack_app.rb
@@ -20,16 +20,19 @@ module Vanilla
       end
     end
     
-    def initialize
+    def initialize(dreamhost_fix=false)
       Soup.prepare
+      @dreamhost_fix = dreamhost_fix
     end
     
     def call(env)
       request = Request.new(env)
       
+      # TODO: this is ugly, but cgi on dreamhost doesn't give anything in path_info
+      uri_path = @dreamhost_fix ? env["SCRIPT_URL"] : request.path_info
+
       # TODO: there must be a better way to get the routing done
-      
-      case request.path_info
+      case uri_path
       when URL_ROOT
         snip = 'start'
         format = 'html'
@@ -42,10 +45,11 @@ module Vanilla
         snip = $1
         part = $2
         format = $4
+      else
+        return [404, {"Content-Type" => "text/html"}, ["Couldn't match path '#{request.path_info}'"]]
       end
     
       params = request.params.merge(:snip => snip, :part => part, :format => format)
-    
       rendered_output = Vanilla.present(params, snip, part)
     
       [200, {"Content-Type" => "text/html"}, [rendered_output]]
