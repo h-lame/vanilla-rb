339ac8aa79137eb35a4e6202ccc5e3893125663d
James Adam <james@lazyatom.com>
2008-03-06 17:31:28 +0000

    Added the static route middleware to the rackup specification.

--- vanilla/rack_app.rb
+++ vanilla/rack_app.rb
@@ -20,7 +20,6 @@ module Vanilla
       request = Request.new(env)
       
       # TODO: there must be a better way to get the routing done
-      # TODO: what about static routes?
       
       case request.path_info
       when URL_ROOT
--- vanilla/vanilla.ru
+++ vanilla/vanilla.ru
@@ -3,4 +3,6 @@ require 'vanilla/rack_app'
 
 Soup.prepare
 
+# TODO: get this working
+use Rack::Static, :urls => ["/public"], :root => "vanilla"
 run Vanilla::RackApp.new
