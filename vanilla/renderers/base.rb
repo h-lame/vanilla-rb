2112d8023da418689a9345b28bdacb6c33331d79
James Adam <james@lazyatom.com>
2008-03-20 17:15:20 +0000

    Move Vanilla::Render::Base into its own file.

---
+++ vanilla/renderers/base.rb
@@ -0,0 +1,53 @@
+module Vanilla
+  module Render
+    class Base
+      attr_reader :context, :snip, :part, :args
+    
+      def initialize(snip, snip_part=:content, context={}, args=[])
+        @context = context
+        @snip = snip
+        @part = snip_part
+        @args = args
+      end
+    
+      # Handles processing the text of the content. Subclasses should
+      # override this method to do fancy text processing like markdown
+      # or loading the content as Ruby code.
+      def process_text(snip, content, args)
+        content
+      end
+    
+      SNIP_REGEXP = re = %r{ \{
+        ([\w\-]+) (?: \.([\w\-]+) )?
+        (?: \s+ ([\w\-,]+) )?
+      \} }x
+    
+      # Default behaviour to include a snip's content
+      def include_snips(content)
+        content.gsub(SNIP_REGEXP) do
+          snip_name = $1
+          snip_attribute = $2
+          snip_args = $3 ? $3.split(',') : []
+          # Render the snip or snip part with the given args, and the current
+          # context, but with the default renderer for that snip.
+          Render.render(snip_name, snip_attribute, @context, snip_args)
+        end
+      end
+    
+      def render_without_including_snips
+        process_text(@snip, raw_content, @args)
+      end
+    
+      # Returns the raw content for the selected part of the selected snip
+      def raw_content
+        @snip.__send__(@part)      
+      end
+    
+      # Default rendering behaviour. Subclasses shouldn't really need to touch this.
+      def render
+        processed_text = process_text(@snip, raw_content, @args)
+        include_snips(processed_text)
+      end
+    end
+  end
+end
