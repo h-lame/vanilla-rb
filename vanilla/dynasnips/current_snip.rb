2fc62b8f4ed5048a6f314ea953cee5f5f96ecb5c
James Adam <james@lazyatom.com>
2008-03-27 17:13:44 +0000

    The current_snip/link_to_current_snip dynasnips dont need to be method-sensitive.

--- vanilla/dynasnips/current_snip.rb
+++ vanilla/dynasnips/current_snip.rb
@@ -1,6 +1,6 @@
 dynasnip "current_snip", %{
   class CurrentSnip < Dynasnip
-    def get(*args)        
+    def handle(*args)        
       if args[0] == 'name'
         if context[:snip] == 'edit' # we're editing so don't use this name
           context[:snip_to_edit]
