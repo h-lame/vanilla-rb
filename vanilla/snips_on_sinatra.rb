f9d8dc29591d2067aa3c8a991451dcbe71e6ca95
Murray Steele <muz@h-lame.com>
2008-04-05 17:47:49 +0100

    Have the sinatra-based vanilla do CGI escaping of snip, part, and format.  This way we can get to snips with spaces etc... in their names, just like we can with Rack. Also changed the README - the rack version is probably more poked at (less code == better code) so we'll make it the default (and note the rack dependency) and mention the sinatra one in the parenthesis.

--- vanilla/snips_on_sinatra.rb
+++ vanilla/snips_on_sinatra.rb
@@ -10,5 +10,19 @@ static('/public', 'vanilla/public')
 Soup.prepare
 
 get('/') { redirect Vanilla::Routes.url_to('start') }
-get('/:snip') { Vanilla.present params }
-get('/:snip/:part') { Vanilla.present params }
+['get', 'post', 'put', 'delete'].each do |method|
+  send(method, '/:snip.:format') do
+    Vanilla.present sort_out_the_params_you_muppet(params, method)
+  end
+  send(method, '/:snip/:part.:format') do
+    Vanilla.present sort_out_the_params_you_muppet(params, method)
+  end
+end
+
+def sort_out_the_params_you_muppet(params ={}, method = 'get')
+  [:snip, :part, :format].each do |url_bit|
+    params[url_bit] = CGI.unescape(params[url_bit]) unless params[url_bit].nil?
+  end
+  params[:method] = method
+  params
+end
