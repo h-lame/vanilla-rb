187a9f4edca00842c2dd4c3c1c972dc0c13005f4
James Adam <james@lazyatom.com>
2008-04-08 22:51:08 +0100

    The pre dynasnip should be able to render specific parts

--- vanilla/dynasnips/pre.rb
+++ vanilla/dynasnips/pre.rb
@@ -2,8 +2,18 @@ require 'vanilla/dynasnip'
 
 class ShowContentInPreTag < Dynasnip
   snip_name "pre"
+
+  usage %|
+    Wraps the contents of the given snip in &lt;pre&gt; tags, e.g.
+    
+      {pre my_snip}
+      
+    You can specify a part to render in pre tags, should you wish:
+    
+      {pre my_snip,specific_part}
+  |
   
-  def handle(snip_name)
-    %{<pre>#{Snip[snip_name].content}</pre>}
+  def handle(snip_name, part=:content)
+    %{<pre>#{Snip[snip_name].__send__(part || :content)}</pre>}
   end
 end
